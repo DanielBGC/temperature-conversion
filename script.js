152225c14d099774ba3b16bf2b43328f9561c1f7
Daniel Borba Guimarães da Costa <daniel.borba-@hotmail.com>
2020-06-27 14:48:53 -0300

    Refatorando o código
    A temperatura convertida é exibida nas 3 unidades, não apenas na que o usuário escolher

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
+var inputs = document.querySelectorAll(".temp-inicial input");
+var divs = document.querySelectorAll("#res div")
+
+inputs.forEach(element => {
+    element.addEventListener('click', converter)
+});
+
 function converter() {
-    var valor = document.getElementById("entrada")
-    var res = document.getElementById("res")
-            
-    var tempInicial = document.getElementsByName("tempInicial")
-    var tempFinal = document.getElementsByName("tempFinal")
-            
+    divs.forEach(element => {
+        element.classList.remove("selected")
+    })
+    
+    let valor = document.getElementById("entrada")
+    let tempInicial = document.getElementsByName("tempInicial")
+    let celsP = document.querySelector(".cels")
+    let fahrP = document.querySelector(".fahr")
+    let kelvinP = document.querySelector(".kelvin")
+
+    let cels;
+    let fahr;
+    let kelvin;
+
+    //temperatura inicial = Celsius
     if(tempInicial[0].checked) { 
-        var cels = Number(valor.value);
+        celsP.parentElement.classList.add("selected")
 
-        if(tempFinal[0].checked) { //temperatura inicial Celsius
-            res.innerHTML = `${cels}ºC = ${cels}ºC`
-        }
+        cels = Number(valor.value);
 
-        if(tempFinal[1].checked) {
-            var fahr = cels*(9/5)+32;
-            res.innerHTML = `${cels}ºC = ${fahr.toFixed(2)}ºF`
-        }
-
-        if(tempFinal[2].checked) {
-            var kelv = cels + 273;
-            res.innerHTML = `${cels}ºC = ${kelv}K`
-        }
+        celsP.innerHTML = `${cels} ºC`
+    
+        fahr = cels*(9/5)+32;
+        fahrP.innerHTML = `${fahr.toFixed(2)} ºF`
+    
+        kelvin = cels + 273;
+        kelvinP.innerHTML = `${kelvin} K`
     }
 
-    if(tempInicial[1].checked) { //temperatura inicial Fahrenheit
-        var fahr = Number(valor.value);
+    //temperatura inicial = Fahrenheit
+    if(tempInicial[1].checked) { 
+        fahrP.parentElement.classList.add("selected")
 
-        if(tempFinal[0].checked) {
-            var cels = (fahr-32)*(5/9)
-            res.innerHTML = `${fahr}ºF = ${cels.toFixed(2)}ºC`
-        }
+        fahr = Number(valor.value);
+        fahrP.innerHTML = `${fahr} ºF`
 
-        if(tempFinal[1].checked) {
-            res.innerHTML = `${fahr}ºF = ${fahr}ºF`
-        }
-
-        if(tempFinal[2].checked) {
-            var kelv = (fahr-32)*(5/9)+273;
-            res.innerHTML = `${fahr}ºF = ${kelv.toFixed(2)}K`
-        }
+        cels = (fahr-32)*(5/9);
+        celsP.innerHTML = `${cels.toFixed(2)} ºC`
+    
+        kelvin = (fahr-32)*(5/9)+273;
+        kelvinP.innerHTML = `${kelvin.toFixed(2)} K`
     }
 
-    if(tempInicial[2].checked) { //temperatura inicial Kelvin
-        var kelv = Number(valor.value);
-        if(kelv >= 0) {
-            if(tempFinal[0].checked) {
-                var cels = kelv - 273;
-                res.innerHTML = `${kelv}K = ${cels}ºC`
-            }
+    //temperatura inicial = Kelvin
+    if(tempInicial[2].checked) { 
+        kelvinP.parentElement.classList.add("selected")
 
-            if(tempFinal[1].checked) {
-                var fahr = (kelv-273)*(9/5)+32;
-                res.innerHTML = `${kelv}K = ${fahr.toFixed(2)}ºF`
-            }
+        kelvin = Number(valor.value);
+        kelvinP.innerHTML = `${kelvin} K`
 
-            if(tempFinal[2].checked) {
-                res.innerHTML = `${kelv}K = ${kelv}K`
-            }
-        }
-        else 
-            res.innerHTML = `[ERRO] Temperatura negativa em Kelvin`
+        cels = kelvin - 273;
+        celsP.innerHTML = `${cels} ºC`
+        
+        fahr = (kelvin-273)*(9/5)+32;
+        fahrP.innerHTML = `${fahr.toFixed(2)} ºF`
     }
 }
